48beaf0f0691d57730b2a0f46fb7da884a53b602
pratham3115 <jpratham134@gmail.com>
2025-03-13 19:23:04 +0530

    all errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the path according to your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
